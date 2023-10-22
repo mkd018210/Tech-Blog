17d5207d773f97b03395d01c60bd0e82a9238330
Mommy Me <mkm0182@yahoo.com>
2023-10-21 20:23:50 -0500

    Added server.js context

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({ helpers: require("./utils/helpers")});
+
+const app = espress();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    sercret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: ture}));
+
+app.use(express.static("public"));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(
+    session({
+        secret: process.env.SECRET,
+        store: new SequelizeStore({ db: sequelize }),
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Listening on PORT ${PORT}'));
+});
